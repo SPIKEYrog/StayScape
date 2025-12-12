84fefa8f1bd60f5f48361bccdc1ac174dc0ac085
sai kiran <saikiran20549@gmail.com>
2025-12-13 03:10:14 +0530

    added the navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
+import Navebar from './components/navbar'
 
 const App = () => {
+  const isOwnewrPath =useLocation().pathname.includes('/owner');
   return (
-    <>
-      <div className="min-h-screen flex items-center justify-center bg-gray-100">
-        <h1 className="text-4xl font-bold text-blue-600">Welcome to StayScape</h1>
-      </div>
-    </>
+    <div>
+      <Navebar />
+    </div>
+    
   )
 }
 
