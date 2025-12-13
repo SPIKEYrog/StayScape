aba5d8e5ae8201f5e77006a003099a8f8b16387d
sai kiran <saikiran20549@gmail.com>
2025-12-13 15:50:08 +0530

    just to verify

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Navebar from './components/navbar'
+import { useLocation } from 'react-router-dom'
 
 const App = () => {
   const isOwnewrPath =useLocation().pathname.includes('/owner');
